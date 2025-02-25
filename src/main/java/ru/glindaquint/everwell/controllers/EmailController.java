5d08a76be67e77036f9e4495a96bdf1de84f28df
glindaqu <q5481996@gmail.com>
2025-02-25 19:21:52 +0700

    requests limit added

--- src/main/java/ru/glindaquint/everwell/controllers/EmailController.java
+++ src/main/java/ru/glindaquint/everwell/controllers/EmailController.java
@@ -1,19 +1,50 @@
 package ru.glindaquint.everwell.controllers;
 
+import io.github.bucket4j.Bucket;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import ru.glindaquint.everwell.services.EmailService;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequestMapping("/email")
 public class EmailController {
-
     @Autowired
     private EmailService emailService;
 
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    private Bucket resolveBucket(String ip) {
+        return buckets.computeIfAbsent(ip, this::createNewBucket);
+    }
+
+    private Bucket createNewBucket(String key) {
+        return Bucket.builder()
+                .addLimit(limit -> limit.capacity(1).refillGreedy(1, Duration.ofMinutes(3)))
+                .build();
+    }
+
     @GetMapping("/send-email")
-    public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String body) {
-        emailService.send(to, subject, body);
-        return "Email sent successfully.";
+    public ResponseEntity<?> sendEmail(
+            @RequestParam String to,
+            @RequestParam String subject,
+            @RequestParam String body,
+            @RequestHeader("X-Forwarded-For") String ip
+    ) {
+        Bucket bucket = resolveBucket(ip);
+        if (bucket.tryConsume(1)) {
+            emailService.send(to, subject, body);
+            return ResponseEntity.ok("Sent");
+        } else {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("Request limit reached");
+        }
     }
 }
